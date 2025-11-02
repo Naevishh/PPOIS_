dc9f052f1a77011309f1f740dd2a7e5cc2f47fbb
sannya <aleksandraasenko804@gmail.com>
2025-11-03 02:43:09 +0300

    Add files to lab1/sem3_l1_ppois

---
+++ lab1/sem3_l1_ppois/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/aleks/CLionProjects/sem3_l1_ppois
+# Build directory: C:/Users/aleks/CLionProjects/sem3_l1_ppois/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("Tests")
+subdirs("Dictionary")
+subdirs("googletest-main")
---
+++ lab1/sem3_l1_ppois/cmake-build-debug/googletest-main/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/aleks/CLionProjects/sem3_l1_ppois/googletest-main/googlemock
+# Build directory: C:/Users/aleks/CLionProjects/sem3_l1_ppois/cmake-build-debug/googletest-main/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
